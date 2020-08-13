2640b71dcec6c421313b310ba71d0f4ee130c4fc
lintao <mrqter@gmail.com>
2020-08-13 12:50:12 +0800

    defined pipline

---
+++ pipline_op.go
@@ -0,0 +1,143 @@
+/*
+ *
+ * pipline_op.go
+ * tugrik
+ *
+ * Created by lintao on 2020/8/13 12:42 下午
+ * Copyright © 2020-2020 LINTAO. All rights reserved.
+ *
+ */
+
+package tugrik
+
+func Abs() {
+}
+
+func Accumulator() {
+}
+
+func Acos() {
+}
+
+func Acosh() {
+}
+
+func Add() {
+}
+
+func AddToSet() {
+}
+
+func AllElementsTrue() {
+}
+
+func And() {
+}
+
+func AnyElementTrue() {
+}
+
+func ArrayElemAt() {
+}
+
+func ArrayToObject() {
+}
+
+func Asin() {
+}
+
+func Asinh() {
+}
+
+func Atan() {
+}
+
+func Atan2() {
+}
+
+func Atanh() {
+}
+
+func Avg() {
+}
+
+func BinarySize() {
+}
+
+func BsonSize() {
+}
+
+func Ceil() {
+}
+
+func Cmp() {
+}
+
+func Concat() {
+}
+
+func concatArray() {
+}
+
+func Cond() {
+}
+
+func Convert() {
+}
+
+func Cos() {
+}
+
+func DateFromParts() {
+}
+
+func DataToString() {
+}
+
+func DayOfMonth() {
+}
+
+func DayOfWeek() {
+}
+
+func DayOfYear() {
+}
+
+func DegreesToRadians() {
+}
+
+func Divide() {
+}
+
+func Eq() {
+}
+
+func Exp() {
+}
+
+func Filter() {
+}
+
+func First() {
+}
+
+func Floor() {
+}
+
+func Function() {
+}
+
+func Gt() {
+}
+
+func Gte() {
+}
+
+func Hour() {
+}
+
+func IsNull() {
+}
+
+func In() {
+}
