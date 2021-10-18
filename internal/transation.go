0e9d47489105a79f4b2109408e99bfdd5ae0998f
lintao <mrqter@gmail.com>
2021-10-19 00:58:05 +0800

    fix transation

--- internal/transation.go
+++ internal/transation.go
@@ -3,9 +3,11 @@ package internal
 import (
 	"context"
 
-	"github.com/5xxxx/pie/schemas"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 
-	"go.mongodb.org/mongo-driver/mongo/writeconcern"
+	"go.mongodb.org/mongo-driver/mongo"
+
+	"github.com/5xxxx/pie/schemas"
 
 	"go.mongodb.org/mongo-driver/mongo/readconcern"
 
@@ -17,16 +19,19 @@ func (d defaultClient) TransactionWithOptions(ctx context.Context, f schemas.Tra
 	if err != nil {
 		return err
 	}
-	if err = f(ctx); err != nil {
-		return session.AbortTransaction(ctx)
-	}
 	defer session.EndSession(context.Background())
-	return session.CommitTransaction(ctx)
+
+	txnOpts := options.Transaction().
+		SetReadPreference(readpref.PrimaryPreferred())
+	_, err = session.WithTransaction(ctx, func(sessCtx mongo.SessionContext) (interface{}, error) {
+		return nil, f(ctx)
+	}, txnOpts)
+
+	return err
 }
 
 func (d defaultClient) Transaction(ctx context.Context, f schemas.TransFunc) error {
 	opts := options.Session().
-		SetDefaultReadConcern(readconcern.Majority()).
-		SetDefaultWriteConcern(writeconcern.New(writeconcern.WMajority()))
+		SetDefaultReadConcern(readconcern.Majority())
 	return d.TransactionWithOptions(ctx, f, []*options.SessionOptions{opts}...)
 }
